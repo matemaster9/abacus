d3fef4cd8619718c22e0b0238328fe53e110187b
matemaster9 <matemaster@163.com>
2023-03-26 22:57:27 +0800

    多态函数-q1

--- src/main/scala/org/mastercs/bigdata/scala_lang/functional/PolymorphicFunction.scala
+++ src/main/scala/org/mastercs/bigdata/scala_lang/functional/PolymorphicFunction.scala
@@ -7,11 +7,16 @@ object PolymorphicFunction {
     def main(args: Array[String]): Unit = {
         val args = Array("I", "am", "a", "coder")
         println(findFirst(args, "a"))
-        println(findAnyFirst[String](args, elem => elem == "a"))
+        println(typeAbstractFindAnyFirst[String](args, elem => elem == "a"))
+        val outOfOrderArr = Array(0, 5, 2, 9, 1)
+        val ascendingArr = Array(1, 2, 3, 4, 5, 6)
+        val descendingArr = Array(6, 5, 4, 3, 2, 1)
+        println(typeAbstractIsSorted[Int](outOfOrderArr, (a, b) => a > b))
+        println(typeAbstractIsSorted[Int](ascendingArr, (a, b) => a > b))
+        println(typeAbstractIsSorted[Int](descendingArr, (a, b) => a < b))
     }
 
     def findFirst(args: Array[String], key: String): Int = {
-
         @tailrec
         def loop(i: Int): Int = {
             if (i >= args.length) -1
@@ -22,7 +27,7 @@ object PolymorphicFunction {
         loop(0)
     }
 
-    def findAnyFirst[A](args: Array[A], func: A => Boolean): Int = {
+    def typeAbstractFindAnyFirst[A](args: Array[A], func: A => Boolean): Int = {
         @tailrec
         def loop(i: Int): Int = {
             if (i >= args.length) -1
@@ -31,5 +36,24 @@ object PolymorphicFunction {
         }
 
         loop(0)
+    }
+
+    def typeAbstractIsSorted[A](args: Array[A], sorted: (A, A) => Boolean): Boolean = {
+
+        def asc(args: Array[A], sorted: (A, A) => Boolean): Boolean = {
+            for (i <- 0 until (args.length - 1)) {
+                if (sorted(args(i), args(i + 1))) return false
+            }
+            true
+        }
+
+        def desc(args: Array[A], sorted: (A, A) => Boolean): Boolean = {
+            for (i <- 0 until (args.length - 1)) {
+                if (sorted(args(i), args(i + 1))) return false
+            }
+            true
+        }
+
+        asc(args, sorted) || desc(args, sorted)
     }
 }
