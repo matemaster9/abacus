28f4d0da37b239fb97587dcf24bfe6da5f7249df
matemaster9 <matemaster@163.com>
2023-03-26 19:41:19 +0800

    模式匹配的探究

---
+++ src/main/scala/org/mastercs/bigdata/scala_lang/traits/Person.scala
@@ -0,0 +1,11 @@
+package org.mastercs.bigdata.scala_lang.traits
+
+class  Person(var name: String, var age: Int) extends Speaker {
+    // 辅助构造函数
+    def this(name: String) {
+        this(name, 0)
+    }
+
+    // 实现特质中的方法
+    def speak(): String = s"My name is $name and I am $age years old."
+}
---
+++ src/main/scala/org/mastercs/bigdata/scala_lang/traits/Speaker.scala
@@ -0,0 +1,5 @@
+package org.mastercs.bigdata.scala_lang.traits
+
+trait Speaker {
+    def speak(): String
+}
