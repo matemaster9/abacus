fc98084ec2473c1d312f4b11e90a5e37300f40b7
matemaster9 <matemaster@163.com>
2023-03-26 20:11:55 +0800

    scalatest

---
+++ src/test/scala/org/mastercs/bigdata/test/SizeSuite.scala
@@ -0,0 +1,15 @@
+package org.mastercs.bigdata.test
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class SizeSuite extends AnyFunSuite {
+
+    test("empty size") {
+        assert(Set.empty.isEmpty)
+    }
+
+
+    test("haha") {
+
+    }
+}
