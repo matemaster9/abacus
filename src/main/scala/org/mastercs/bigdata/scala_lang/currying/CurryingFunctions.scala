4d27e6d324454d77f93788edb2ff0dd23f28252f
matemaster9 <matemaster@163.com>
2023-03-27 11:48:54 +0800

    函数柯里化

---
+++ src/main/scala/org/mastercs/bigdata/scala_lang/currying/CurryingFunctions.scala
@@ -0,0 +1,19 @@
+package org.mastercs.bigdata.scala_lang.currying
+
+object CurryingFunctions extends App {
+
+    val superAdder: Int => Int => Int = x => y => (x + y)
+    println(superAdder(3)(5))
+
+    val add100 = superAdder(100)
+    println(add100(1))
+
+
+    def curriedFunc(x: Int)(y: Int): Int = {
+        x + y
+    }
+
+    val add10000: Int => Int = curriedFunc(10000)
+    println(add10000(1))
+    println(curriedFunc(10000)(1))
+}
