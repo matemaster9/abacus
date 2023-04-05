4c46e681cbb2326d134b39409fb5264b6a846e71
matemaster9 <matemaster@163.com>
2023-04-05 16:00:22 +0800

    常规排序

--- src/main/java/org/mastercs/leetcode/Sol2427.java
+++ src/main/java/org/mastercs/leetcode/Sol2427.java
@@ -1,5 +1,7 @@
 package org.mastercs.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author HUAWEI
  * @date 2023/4/5 6:25
@@ -15,5 +17,40 @@ public class Sol2427 {
             }
         }
         return count;
+    }
+
+    public static int binarySearch(int[] arr, int key, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            return binarySearch(arr, key, mid + 1, high);
+        } else {
+            return binarySearch(arr, key, low, mid - 1);
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 7, 1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
