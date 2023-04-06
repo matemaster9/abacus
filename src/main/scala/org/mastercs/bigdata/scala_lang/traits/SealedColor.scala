db3990fcdc5b6abc1a13d0de3bde7109ff04d155
matemaster9 <matemaster@163.com>
2023-04-06 18:36:42 +0800

    scala特性

--- src/main/scala/org/mastercs/bigdata/scala_lang/traits/SealedColor.scala
+++ src/main/scala/org/mastercs/bigdata/scala_lang/traits/SealedColor.scala
@@ -1,22 +1,24 @@
 package org.mastercs.bigdata.scala_lang.traits
 
+/**
+ * sealed class 必须与子类型在统一文件
+ */
 sealed trait SealedColor {
 
 
 }
 
+case object Red extends SealedColor
+
+case object Pink extends SealedColor
+
+case object Blue extends SealedColor
+
 object SealedColor extends App {
-
-    case object Red extends SealedColor
-
-    case object Pink extends SealedColor
-
-    case object Blue extends SealedColor
 
     def printSealedColor(color: SealedColor): Unit = color match {
         case Red => println("红色")
         case Pink => println("粉色")
         case Blue => println("蓝色")
-        case _ => println("未知颜色")
     }
 }
