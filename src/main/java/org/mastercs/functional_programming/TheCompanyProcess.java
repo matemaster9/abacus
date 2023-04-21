8ad4cfe69d83930e3c1eeb90d529f7c57e4b12dd
matemaster9 <matemaster@163.com>
2023-04-21 21:04:02 +0800

    函数式编程思维

---
+++ src/main/java/org/mastercs/functional_programming/TheCompanyProcess.java
@@ -0,0 +1,35 @@
+package org.mastercs.functional_programming;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author matemaster
+ */
+public class TheCompanyProcess {
+
+    public static String cleanNames(List<String> nameList) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : nameList) {
+            if (s != null && s.length() > 1) {
+                builder.append(capitalizeString(s)).append(",");
+            }
+        }
+        return builder.substring(0, builder.length() - 1).toString();
+    }
+
+
+    public static String cleanNamesByFunctionalProgramming(List<String> nameList) {
+        if (nameList == null || nameList.size() == 0) {
+            return null;
+        }
+        return nameList.stream()
+                .filter(str -> str != null && str.isEmpty())
+                .map(TheCompanyProcess::capitalizeString)
+                .collect(Collectors.joining(","));
+    }
+
+    public static String capitalizeString(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+}
--- src/test/scala/org/mastercs/scala_lang/test/functional_programming/FunctionalProgrammingSuite.scala
+++ src/test/scala/org/mastercs/scala_lang/test/functional_programming/FunctionalProgrammingSuite.scala
@@ -1,6 +1,6 @@
 package org.mastercs.scala_lang.test.functional_programming
 
-import org.mastercs.bigdata.scala_lang.functional_programming.FuncList
+import org.mastercs.bigdata.scala_lang.functional_programming.{FuncList, TheCompanyProcess}
 import org.mastercs.bigdata.scala_lang.functional_programming.FuncList.sum
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -8,5 +8,9 @@ class FunctionalProgrammingSuite extends AnyFunSuite {
 
     test("函数式数据结构：FuncList") {
         println(sum(FuncList(1, 2, 3, 4, 5)))
+    }
+
+    test("TheCompanyProcess") {
+        println(TheCompanyProcess.cleanNamesByFunctionalProgramming(List("mate", "master")))
     }
 }
