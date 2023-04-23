a29b7f3f11b929494b56d37111bd7941d1a5b16c
mastercs <matemaster@163.com>
2023-04-23 10:57:22 +0800

    代码优化

--- src/main/java/org/mastercs/bigdata/functional_thinking/JavaStreamNaturalNumberClassifier.java
+++ src/main/java/org/mastercs/bigdata/functional_thinking/JavaStreamNaturalNumberClassifier.java
@@ -5,9 +5,11 @@ import java.util.stream.IntStream;
 public class JavaStreamNaturalNumberClassifier {
 
     public static int aliquotSumOf(int targetNumber) {
-        return IntStream.range(1, targetNumber + 1)
-                .filter(potential -> targetNumber % potential == 0)
-                .sum();
+        return IntStream.range(1, targetNumber + 1).filter(potential -> isFactor(targetNumber, potential)).sum();
+    }
+
+    public static boolean isFactor(final int candidate, final int number) {
+        return number % candidate == 0;
     }
 
     public static boolean isPerfect(final int number) {
